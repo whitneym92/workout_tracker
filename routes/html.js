f417dd63cfdaba44c7c7e7f747baf94e3f44a6e6
Whitney Monk <whitneym92@gmail.com>
2020-09-25 11:40:29 -0500

    fixed bug

--- routes/html.js
+++ routes/html.js
@@ -6,12 +6,12 @@ router.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-router.get("/exercise", (req,res) =>{
+router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-router.get("/stats", (req,res => {
+router.get("/stats", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
-}));
+});
 
 module.exports = router;
