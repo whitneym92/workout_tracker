df82207adb15804afa2abb7c62d17d96e1d00078
Whitney Monk <whitneym92@gmail.com>
2020-09-25 11:00:45 -0500

    updated mongoAtlas connection

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/fitness',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
 const apiRoutes = require("./routes/api.js");
 const htmlRoutes = require("./routes/html.js");
 
