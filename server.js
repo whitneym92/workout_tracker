b49f66746f2026209c58088610a4e5560886defc
Whitney Monk <whitneym92@gmail.com>
2020-09-25 12:01:51 -0500

    updated app.use

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan"); 
+
+const apiRoutes = require("./routes/api.js");
+const htmlRoutes = require("./routes/html.js");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const db = require("./models");
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use(htmlRoutes);
+app.use("/api", apiRoutes);
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/fitness',
@@ -19,8 +29,7 @@ mongoose.connect(
     }
   );
 
-const apiRoutes = require("./routes/api.js");
-const htmlRoutes = require("./routes/html.js");
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
